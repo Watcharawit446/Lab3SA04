69f3cd6906ec6ce51bba7af7c19bc9ed7c9012db
Watcharawit Saejung <6310110446@psu.ac.th>
2022-08-24 23:58:37 +0700

    commit checkpoint 2

---
+++ components/Weather.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Text, ImageBackground, View } from 'react-native'
+import { StyleSheet } from 'react-native-web'
+import Forecast from './Forcast'
+
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        })
+
+    return (
+        <View>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+         <Text>Zip Code</Text>
+        <Text>{props.zipCode}</Text>
+        <Forecast {...forecastInfo} />
+        </ImageBackground>
+         </View>
+    )
+}
+
+const styles = StyleSheet.create({
+ backdrop: {
+ alignItems: 'center',
+ width: '100%',
+ height: '100%'
+ }
+});

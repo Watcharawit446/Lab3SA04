54d3245914c2e207242fcadf3b65497675b036b4
Watcharawit Saejung <6310110446@psu.ac.th>
2022-08-24 22:27:11 +0700

    commit checkpoint 1

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );

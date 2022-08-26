8777b401126a7cf57e84d2d59c3b0c2de226aff2
Watcharawit Saejung <6310110446@psu.ac.th>
2022-08-26 21:55:16 +0700

    add Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, ImageBackground, View } from 'react-native'
 import { StyleSheet } from 'react-native-web'
 import Forecast from './Forcast'
@@ -7,10 +7,29 @@ import Forecast from './Forcast'
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-        main: 'main',
-        description: 'description',
+        main: '',
+        description: '',
         
         })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+        .then((response) => response.json())
+        .then((json) => {
+        setForecastInfo({
+        main: json.weather[0].main,
+        description: json.weather[0].description,
+        temp: json.main.temp
+        });
+        })
+        .catch((error) => {
+        console.warn(error);
+        });
+        }
+        }, [props.zipCode])
+    
 
     return (
         <View >
